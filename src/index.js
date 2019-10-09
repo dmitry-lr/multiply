121ac7a4cf06ffce01ef008281c1e99e24553e16
Dmitry Rodygin <dmitrylrodygin@gmail.com>
2019-10-09 23:26:57 +0300

    init: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  var aa = first.split('').reverse();
+  var bb = second.split('').reverse();
+
+  var stack = [];
+
+  for (var i = 0; i < aa.length; i++) {
+    for (var j = 0; j < bb.length; j++) {
+      var m = aa[i] * bb[j];
+      stack[i + j] = (stack[i + j]) ? stack[i + j] + m : m;
+    }
+  }
+
+  for (var i = 0; i < stack.length; i++) {
+    var num = stack[i] % 10;
+    var move = Math.floor(stack[i] / 10);
+    stack[i] = num;
+
+    if (stack[i + 1])
+      stack[i + 1] += move;
+    else if (move != 0)
+      stack[i + 1] = move;
+  }
+
+
+  return stack.reverse().join('');
 }
